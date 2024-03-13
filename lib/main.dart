8f27cc588c4c8f7209018fd7628835b0de53d0a1
Saurabh Maurya <saurabhmaurya2050@gmail.com>
2024-03-13 17:04:10 +0530

    making product details pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/global_variable.dart';
 import 'package:shop_app/home.dart';
 import 'package:shop_app/product_details_page.dart';
 
@@ -19,6 +20,11 @@ class MyApp extends StatelessWidget {
           seedColor: const Color.fromRGBO(254, 206, 1, 1),
           primary: const Color.fromRGBO(254, 206, 1, 1),
         ),
+        appBarTheme: const AppBarTheme(
+            titleTextStyle: TextStyle(
+          fontSize: 20,
+          color: Colors.black,
+        )),
         inputDecorationTheme: const InputDecorationTheme(
           hintStyle: TextStyle(
             fontWeight: FontWeight.bold,
@@ -26,14 +32,21 @@ class MyApp extends StatelessWidget {
           ),
           prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
         ),
-        textTheme:const TextTheme(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 35,
+          ),
           titleMedium: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 20,
-          )
-        )
+          ),
+        ),
+        useMaterial3: true,
       ),
-      home: const ProductDetails(),
+      home:  ProductDetails(
+        product: products[0],
+      ),
     );
   }
 }
--- lib/product_details_page.dart
+++ lib/product_details_page.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
 
 class ProductDetails extends StatelessWidget {
-  const ProductDetails({super.key});
+  final Map<String, Object> product;
+  const ProductDetails({
+    super.key,
+    required this.product,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Details'),
+      ),
+      body: Column(
+        children: [
+          Text(
+            product['title'] as String,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(product['imageUrl'] as String),
+          ),
+          const Spacer(
+            flex: 2,
+          ),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(245, 247, 249, 1),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  '\$${product['price']}',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ListView.builder(
+                  itemCount: (product['sizes'] as List<double>).length,
+                  itemBuilder: (context, index) {
+                    final size = 
+                  })
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
